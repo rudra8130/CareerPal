a4cdeba8c2af5db2dfaac9860c5d85a672d068fe
Rudra Bhardwaj <rudra.knit81@gmail.com>
2026-09-19 11:16:05 +0530

    fixed mongodb

--- Backend/services/roadmap/configs/db.js
+++ Backend/services/roadmap/configs/db.js
@@ -5,7 +5,7 @@ dns.setServers(['1.1.1.1', '8.8.8.8'])
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URL, { family: 4 })
         console.log("Database Connected")
     }
     catch (error) {
